3eba87b45788a04391368226656f64ee39fd446a
ashish <subediashish35@gmail.com>
2020-09-22 09:44:01 +0545

    Add data directory

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import numpy as np
+import tensorflow
+import pickle
+import re
 
+# Input sentence
+sentence = ["Do not ever come back to me, understood?"]
+
+# Loading class
+classNames = np.load("./data/class_names.npy")
--- train.py
+++ train.py
@@ -64,7 +64,6 @@ y = encoder.fit_transform(y)
 # Split train and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
-
 # Tokenize sentences to numbers
 tokenizer = Tokenizer(num_words=10000)
 tokenizer.fit_on_texts(X_train)
@@ -92,7 +91,6 @@ with open('../glove.6B.100d.txt', encoding="utf8") as glove_file:
 
 
 embedding_matrix = np.zeros((vocab_size, 100))
-print(tokenizer.word_index.items())
 
 for word, index in tokenizer.word_index.items():
     embedding_vector = embeddings_dictionary.get(word)
@@ -102,7 +100,7 @@ print("INFO:- Creating Model")
 
 # LSTM Model with bidirectional layer
 model = Sequential([
-    Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=maxlen, trainable=False),
+    Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=max_len, trainable=False),
     Bidirectional(LSTM(50, dropout=0.2, recurrent_dropout=0.2, return_sequences=True)),
     Bidirectional(LSTM(54, dropout=0.3, recurrent_dropout=0.3, return_sequences=True)),
     Bidirectional(LSTM(60, dropout=0.3, recurrent_dropout=0.3)),
@@ -110,14 +108,17 @@ model = Sequential([
     Dense(7, activation="softmax")
 ])
 
-# compile model
-model.compile(optimizer="adam", loss='categorical_crossentropy', mectrics=['accuracy'])
-print("INFO: Starting Training")
 
-# Train model
-history = model.fit(X_train, y_train, batch_size=128, epochs=1, verbose=1, validation_split=0.2)
+# # compile model
+# model.compile(optimizer="adam", loss='categorical_crossentropy', mectrics=['accuracy'])
+# print("INFO: Starting Training")
 
-# Save model, classes and tokenizer file
-model.save("model_final.model")
+
+
+# # Train model
+# history = model.fit(X_train, y_train, batch_size=128, epochs=1, verbose=1, validation_split=0.2)
+#
+# # Save model, classes and tokenizer file
+# model.save("model_final.model")
 
 
