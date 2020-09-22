06704a9de63e3eca49d2e3f5d391962734d270be
ashish <subediashish35@gmail.com>
2020-09-22 10:00:46 +0545

    Train and store model

--- test.py
+++ test.py
@@ -9,4 +9,8 @@ sentence = ["Do not ever come back to me, understood?"]
 # Loading Emotion types and classes
 classNames = np.load("./data/class_names.npy")
 
+print("[INFO]: Loading Tokens")
+with open('./data/tokenizer.pickle', 'rb') as handle:
+        Tokenizer = pickle.load(handle)
 
+print(Tokenizer.texts_to_sequences(sentence))
